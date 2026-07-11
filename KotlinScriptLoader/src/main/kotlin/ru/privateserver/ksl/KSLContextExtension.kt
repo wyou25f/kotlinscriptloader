39455be888b8e8e19d5790d94d1100c5916e276a
wyou25f <wyou25f@example.com>
2026-07-11 09:05:24 +0300

    1.1.0 early 3

--- KotlinScriptLoader/src/main/kotlin/ru/privateserver/ksl/KSLContextExtension.kt
+++
@@ -1,7 +0,0 @@
-package ru.privateserver.ksl
-
-interface KSLContextExtension {
-    val extensionId: String
-    fun onContextCreated(context: BukkitScriptContext)
-    fun onContextDestroyed(scriptName: String)
-}
